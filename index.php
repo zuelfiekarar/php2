c36903e1363e680fbfff042e8679aff1e4cc21d0
zuelfiekarar <zuelfiekarar@gmail.com>
2024-01-02 23:58:55 +0700

    Revisi first post

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+$koneksi = mysqli_connect("localhost", "root", "", "siam");
+//cek koneksi
+if(!$koneksi){
+    die("Error Koneksi: ".mysqli_connect_errno());
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
